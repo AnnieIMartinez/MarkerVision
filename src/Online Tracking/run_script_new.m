750164eec2557565a2d92dc06b30d03bdf5b7237
arunniddish <63624213+arunniddish@users.noreply.github.com>
2026-01-25 19:49:07 -0600

    Refactor online tracking script for clarity and efficiency

--- src/Online Tracking/run_script_new.m
+++ src/Online Tracking/run_script_new.m
@@ -2,12 +2,8 @@ clear all;
 clc;
 
 %[] Video file information
-vid_name = 'demo_video.mp4'; % Raw video data after configuring camera properties 
-output_vid_filename = 'demo_video_gcf.mp4'; % Final tracked video 
-
-% Instantiate a video objects for this video.
-params.vwrite = VideoWriter(output_vid_filename,'MPEG-4');
-open(params.vwrite);
+vid_name = 'demo_video.mp4'; % Raw video data after configuring camera properties
+output_vid_filename = 'demo_video_gcf.mp4'; % Final tracked video
 
 % Tracking parameters
 params.number_of_markers = 4;
@@ -27,14 +23,12 @@ choose_cam_prop = menu("Adjust Camera properties",'Yes','No');
 if choose_cam_prop == 1
     cam = camera_properties_marker_1920_Blue(cam);
     cam.Resolution = '1920x1080';
-	pause(2);
+    pause(2);
 end
 
-% Distortion parameter
-% Uncomment to use it but create your own. Check paper Sec III-A for how to create one.
-% This is not mandatory for the tracking framework to run. This just corrects for distortion which
-% are not that significant.
-% distortion = load('cameraParams.mat');  
+clear cam;
+
+pause(2);
 
 % Initilaize object for online tracking
 tracker_obj = OnlineTracking(params);
@@ -42,50 +36,46 @@ PrevPt = [];
 P0 = [];
 robo_centroid = [];
 
-input('Press <Enter> to start');
+% Distortion parameter
+% Uncomment to use it but create your own. Check paper Sec III-A for how to create one.
+% This is not mandatory for the tracking framework to run. This just corrects for distortion which
+% are not that significant.
+% distortion = load('cameraParams.mat');
+
+%[] Initilaize object for online tracking
+
+%[] Initialize Parallel Processing --> parfeval-(f)
+parpool('local',1); % It is better if number of pools opened is equal to number of asynchronous function running
 
 %[] Initialize --> Pollable Data Queue-(P)
 P = parallel.pool.PollableDataQueue;
-L = parallel.pool.PollableDataQueue;
-Q = parallel.pool.PollableDataQueue;
 
-
-%[] Initialize --> parfeval-(f)
-parpool('local',1);
-%freq = 25;
 %f = parfeval(@getFrameFromCamera,0,P,L,distortion,vid_name);  % Uncomment and use this if you have distortion parameters. Also uncomment in @getFrameFromCamera
-f = parfeval(@getFrameFromCamera,0,P,L,vid_name);
-pause(1.5);
+f_1 = parfeval(@getFrameFromCamera, 0, P, vid_name);
+
+pause(2);
+
+input('Press <Enter> to start');
 
 idx = 0;
 
 while true
-
-	if P.QueueLength > 0
-	
-		cam_data = poll(P);
+    
+    if P.QueueLength > 0
+     
+        cam_data = poll(P);
         newim = cam_data.img;
         ts = cam_data.time_stamp;
-        poll_data = poll_data + 1;
         idx = idx + 1;
         [output_data] = tracker_obj.tracking(newim,PrevPt,P0,robo_centroid,trajectory_position,idx);
         tracking_data(idx,:) = [output_data.tracking_data(idx,:) ts];
         PrevPt = reshape(output_data.tracking_data(idx,1:12),[3,4])';
         robo_centroid = output_data.robo_centroid;
-		
-		if idx == 1
+
+        if idx == 1
             P0 = output_data.P0;
         end
-        
+
     end
-plot_data.img = newim;
-plot_data.centroids = robo_centroid; 
-plot_data.marker_centroid = PrevPt; 
-send(Q, plot_data);
+
 end
-
-
-
-
-
-
